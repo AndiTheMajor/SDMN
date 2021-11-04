fb26561f24dd5ee03151d31b281c9e8b5c4252a7
AndiTheMajor <89019783+AndiTheMajor@users.noreply.github.com>
2021-11-04 20:09:27 +0100

    Update p.js

--- commands/p.js
+++ commands/p.js
@@ -61,8 +61,15 @@ module.exports = {
             .on('finish', () =>{
                 voiceChannel.leave();
             });
+
+            let ballembed2 = new Discord.MessageEmbed()
+            .setAuthor(`PLAY`)
+            .setColor("RANDOM")
+            .addField(`:thumbsup: Now Playing ***${video.title}***`)
+
+            message.channel.send(ballembed2);
  
-            await message.reply(`:thumbsup: Now Playing ***${video.title}***`)
+        
         } else {
             message.channel.send('No video results found');
         }
