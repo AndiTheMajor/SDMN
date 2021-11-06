51067484bcc60f3de873d23afc80a179527545df
AndiTheMajor <89019783+AndiTheMajor@users.noreply.github.com>
2021-11-06 13:45:28 +0100

    Update leave.js

--- commands/leave.js
+++ commands/leave.js
@@ -6,6 +6,14 @@ module.exports = {
  
         if(!voiceChannel) return message.channel.send("You need to be in a voice channel to stop the music!");
         await voiceChannel.leave();
+
+        let song = new Discord.MessageEmbed()
+        .setAuthor(`SDMN`)
+        .setColor("WHITE")
+        .addField('Izasao sam iz kanala :smiling_face_with_tear:')
+        
+        message.channel.send(song);
+        
         await message.channel.send('Leaving channel :smiling_face_with_tear:')
  
     }
