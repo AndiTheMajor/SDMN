e217a48e9f831197ba9d1a78fd9241de3a25e24b
AndiTheMajor <89019783+AndiTheMajor@users.noreply.github.com>
2022-01-17 14:39:03 +0100

    2

--- commands/music/leave.js
+++ commands/music/leave.js
@@ -12,12 +12,22 @@ module.exports = {
 		},
 	},
 	pokreni: async (client, message, args, cfg, Discord) => {
+		const serverQueue = client.queue.get(message.guild.id);
+
+        if(!message.member.voice.channel) {
+            return message.channel.send(":x: **You have to be in a voice channel to use this command.**")
+        }
         
-		const voiceChannel = message.member.voice.channel;
- 
-        if(!voiceChannel) return message.channel.send("You need to be in a voice channel to stop the music!");
-        await voiceChannel.leave();
-        await message.channel.send('Leaving channel :smiling_face_with_tear:')
+        if(!message.guild.me.voice.channel) {
+            return message.channel.send(":x: **I am not connected to a voice channel**, Use the summon command to get me in one")
+        }
+      
+		serverQueue.songs = [];
+		client.queue.delete(message.guild.id);
+
+        serverQueue.connection.dispatcher.();
+        message.guild.me.voice.channel.leave();
+        message.channel.send("**:mailbox_with_no_mail: Successfully disconnected**");
     
   }
 }
